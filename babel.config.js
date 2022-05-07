35829f07bf9c7a39b7b4c6373767c62b7f7f1fa9
jardenna <hbjornum@gmail.com>
2022-05-07 10:51:32 +0200

    Removed unused code babel.config

--- babel.config.js
+++ babel.config.js
@@ -5,19 +5,5 @@ module.exports = {
     // in files only using JSX (no state or React methods)
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    // ['module-resolver', {
-    //    'root': ['./'],
-    //    'alias': {
-    //       '@formElements': './src/js/components/common/formElements',
-    //       '@utils': './src/js/utils',
-    //       '@commonReact': './src/js/components/common',
-    //       '@hooks': './src/js/hooks',
-    //       '@components': './src/js/components',
-    //       '@context': './src/js/context'
-
-    //    }
-    // }]
-  ],
+  plugins: ['@babel/plugin-transform-runtime'],
 };
