1ba285bb4af8588f60cb841b0714c71c08bc1101
jardenna <hbjornum@gmail.com>
2022-06-02 16:15:53 +0200

    Removed props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
 
-import DocIcon from './Doc';
-
 interface HomeProps {
   title: string;
 }
@@ -10,12 +8,6 @@ const Home: FC<HomeProps> = ({ title }) => {
   return (
     <article>
       <h1>{title}</h1>
-      <div className="doc">
-        <a href="#">
-          <DocIcon />
-          Mit Document
-        </a>
-      </div>
     </article>
   );
 };
