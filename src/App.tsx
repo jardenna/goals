fade2b4e44da39a6265e1a89d850657898b4b429
jardenna <hbjornum@gmail.com>
2022-06-30 06:35:32 +0200

    add month

--- src/App.tsx
+++ src/App.tsx
@@ -18,3 +18,26 @@ function App() {
 }
 
 export default App;
+
+// import { addMonths, subMonths } from "date-fns";
+
+// const date = new Date();
+// function getFirstDayOfMonth(year, month) {
+//   return new Date(year, month, 1);
+// }
+// const firstDayCurrentMonth = getFirstDayOfMonth(
+//   date.getFullYear(),
+//   date.getMonth(),
+// );
+// const a = addMonths(firstDayCurrentMonth, 2);
+// const b = subMonths(firstDayCurrentMonth, 1);
+// document.getElementById("app").innerHTML = `
+
+// <div>
+// Subtract: ${b}
+// </div>
+
+// <div>
+// Add: ${a}
+// </div>
+// `;
