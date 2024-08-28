8dcd63fd65857b03d9c40c1db4882e090003840f
jardenna <hbjornum@gmail.com>
2024-08-28 11:04:43 +0200

    update to Vite

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env',
-    // Runtime automatic with React 17+ allows not importing React
-    // in files only using JSX (no state or React methods)
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
-  plugins: ['@babel/plugin-transform-runtime'],
-};
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      overlay: {
+        initialIsOpen: false, // Disables overlay by default
+      },
+    }),
+    eslint({
+      emitWarning: true,
+    }),
+  ],
+  server: {
+    port: 4000,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    sourcemap: true,
+  },
+});
